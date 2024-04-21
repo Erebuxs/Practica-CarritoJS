f4c02052f13415ab755acc6cfd04c457a07a580f
pc <carlos.amores19@outlook.com>
2024-04-21 11:31:26 -0600

    Leer los datos del curso que seleccionamos

--- js/app.js
+++ js/app.js
@@ -1,1 +1,33 @@
+const carrito = document.querySelector('#carrito');
+const contenedorCarrito = document.querySelector('#lista-carrito tbody');
+const vaciarCarritoBoton = document.querySelector('#vaciar-carrito');
+const listaCurso = document.querySelector('#lista-cursos');
+
+cargarEventListeners();
+function cargarEventListeners(){
+  listaCurso.addEventListener('click', agregarCurso);
+};
+
+function agregarCurso(e) {
+    e.preventDefault();
+    if(e.target.classList.contains('agregar-carrito')){
+        const cursoSeleccionado = e.target.parentElement.parentElement;
+        leeDatosCurso(cursoSeleccionado);
+    }
+};
+
+function leeDatosCurso(curso){
+    console.log(curso);
+
+    const infoCurso = {
+        imagen: curso.querySelector('img').src,
+        titulo: curso.querySelector('h4').textContent,
+        precio: curso.querySelector('.precio span').textContent,
+        id: curso.querySelector('a').getAttribute('data-id'),
+        cantidad: 1
+    };
+
+    console.log(infoCurso);
+}
+
 
