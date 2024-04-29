7cf7765f42bb3157fb428b348ee1db88c1958821
amores <carlos.amores19@outlook.com>
2024-04-28 23:30:56 -0600

    mostrando el resto de la informacion en el carrito

--- js/app.js
+++ js/app.js
@@ -39,12 +39,25 @@ function carritoHTML(){
     limpiarHTML()
 
     articulosCarrito.forEach( curso => {
+        const {imagen, titulo, precio, cantidad, id} = curso;
         const row = document.createElement('tr');
         row.innerHTML = `
             <td>
-                ${curso.titulo}
+                <img src="${imagen}" width="100">
             </td>
-        `;
+            <td>
+                ${titulo}
+            </td>
+            <td>
+                ${precio}
+            </td>
+            <td>
+                ${cantidad}
+            </td>
+            <td>
+                <a href="#" class="borrar-curso" data-id="${id}"> X </a>
+            </td>
+            `;
 
         // agrega html del carrito en el tbody
         contenedorCarrito.appendChild(row)
